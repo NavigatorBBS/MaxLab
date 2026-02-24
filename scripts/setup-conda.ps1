90cd0fcabd6f5767d642bd301fc6fd7e4df6c9fb
chlandori <38585729+chlandori@users.noreply.github.com>
2026-02-24 17:05:42 -0500

    cleanup container resources

--- scripts/setup-conda.ps1
+++
@@ -1,8 +0,0 @@
-. "$(Join-Path $PSScriptRoot "common.ps1")"
-
-Show-BbsHeader -Title "MaxLab Setup - Conda Config"
-
-Add-MinicondaToPath
-Test-CondaAvailable
-Enable-CondaInSession
-Set-CondaChannel
